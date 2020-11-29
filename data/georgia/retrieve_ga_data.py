98f50bfa58d70ca792df11da8095a4ab54579737
belicc <belicc@amazon.com>
2020-11-28 21:49:16 -0500

    initial changes to format_data for ga data and updated retrieve_ga_data to retrieve data by race, files not uploading due to Errno2 error, so manually uploaded 2019 to work with for now

--- data/georgia/retrieve_ga_data.py
+++ data/georgia/retrieve_ga_data.py
@@ -21,22 +21,30 @@ class RetrieveGeorgiaData:
 
         self.year_mapping = {"12":"2012", "13":"2013", "14":"2014", "15":"2015", "16":"2016", "17":"2017", "18":"2018", "19":"2019",  "20":"2020",  "21":"2020"}
         self.valid_summaries = {"so": "Statewide County Totals"}
+        self.valid_levels = {"g": {"grouping":"Grade", "pack":"enrollgrade", "level":"ALLSYS"}, "e": {"grouping":"Ethnicity","pack":"ethnicsex_pub", "level":"LEA" } }
 
-    def download_data(self, years, select_summaries, select_quarters):
+        # self.valid_groupings = {"e": "Ethnicity", "s": "Gender", "g": "Grade", "se": "Gender and Ethnicity",
+        #                      "gi": "Grade and Ethnicity", "gs": "Grade and Gender"}
+
+    def download_data(self, years, select_summaries, select_quarters, select_levels):
         urls_to_retrieve = []
         #input: list of years, list of summaries, list of quarters
 
         for year in years:
             for summary in select_summaries:
+                for level in select_levels:
 
-                human_year = self.year_mapping[year]
-                human_summary = self.valid_summaries[summary]
-                folder_location = "{year}/{summary}".format(year=human_year, summary=human_summary)
 
-                for quarter in select_quarters:
 
-                    new_url = self.create_url(human_year, quarter)
-                    urls_to_retrieve.append((folder_location, new_url))
+                    human_year = self.year_mapping[year]
+                    human_summary = self.valid_summaries[summary]
+                    human_group = self.valid_levels[level]["grouping"]
+                    folder_location = "{year}/{summary}/{group}".format(year=human_year, summary=human_summary, group=human_group)
+
+                    for quarter in select_quarters:
+
+                        new_url = self.create_url(human_year, quarter, level)
+                        urls_to_retrieve.append((folder_location, new_url))
 
         self.write_urls(urls_to_retrieve)
 
@@ -55,7 +63,6 @@ class RetrieveGeorgiaData:
 
         try:
             path, url = folder_url
-            print("Writing:", path, url)
             dest_folder = "./" + path
             if not os.path.exists(dest_folder):
                 os.makedirs(dest_folder)  # create folder
@@ -63,7 +70,6 @@ class RetrieveGeorgiaData:
             r = requests.get(url, allow_redirects=True)
             filename = r.headers.get('content-disposition').split("\"")
             name = filename[0][9:]
-            print("name:", name)
 
             mycontent = "\n".join(str(r.content, 'utf-8').split("\n")[4:])
 
@@ -75,29 +81,30 @@ class RetrieveGeorgiaData:
 
 
 
-    def create_url(self, year, quarter):
+    def create_url(self, year, quarter, level):
         # returns a url in the following format
 
-        start_url = "https://oraapp.doe.k12.ga.us/ows-bin/owa/fte_pack_enrollgrade.download_allsys"
-        fiscal_year_download = "?p_fiscal_year={}{}&p_system_id=ALL&p_report_level=ALLSYS".format(year, quarter)
+        base_url = "https://oraapp.doe.k12.ga.us/ows-bin/owa/"
+        download_url = "fte_pack_{}.download_allsys".format(self.valid_levels[level]["pack"])
+        fiscal_year_url = "?p_fiscal_year={}{}&p_system_id=ALL".format(year, quarter)
+        report_level_url = "&p_report_level={}".format(self.valid_levels[level]["level"])
 
-        return start_url + fiscal_year_download 
+        return base_url + download_url + fiscal_year_url + report_level_url
 
 # Test Cases for create_url
 datahelper = RetrieveGeorgiaData()
 
-# assert to prevent regression issues.
-assert datahelper.create_url("21", 1) == "https://oraapp.doe.k12.ga.us/ows-bin/owa/" \
-                                                "fte_pack_enrollgrade.download_allsys?" \
-                                                "p_fiscal_year=211&p_system_id=ALL&p_report_level=ALLSYS"
 
+# assert to prevent regression issues.
+assert datahelper.create_url("21", 1, "g") == "https://oraapp.doe.k12.ga.us/ows-bin/owa/" \
+                                               "fte_pack_enrollgrade.download_allsys" \
+                                               "?p_fiscal_year=211&p_system_id=ALL" \
+                                               "&p_report_level=ALLSYS"
 # Downloads the Following Years:
 
 test_years = ["12", "13", "14", "15", "16", "17", "18", "19", "20", "21"]
-#select county totals only
 test_select_summaries = ["so"]
 test_select_quarters = [1, 3]
+test_levels = ["e"]
 
-
-datahelper.download_data(test_years, test_select_summaries, test_select_quarters)
-
+datahelper.download_data(test_years, test_select_summaries, test_select_quarters, test_levels)
